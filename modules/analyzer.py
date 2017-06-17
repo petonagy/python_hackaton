0de578616e365f74d1f2c796290300660d4de133
m1n0 <mino.lukac@gmail.com>
2017-06-17 12:56:07 +0200

    Analyzer correct terminology

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -1,10 +1,10 @@
 from modules.datamodel import MetaArticle
 
 class Analyzer(object):
-    def __init__(self, articles: list[list[MetaArticle]]):
-        self.articles = articles
+    def __init__(self, feeds: list[list[MetaArticle]]):
+        self.feeds = feeds
 
-    def get_keywords_count(self) -> list:
+    def get_keywords_count(self) -> dict:
         """
         :return: dict
          {
@@ -16,10 +16,16 @@ class Analyzer(object):
             }
         }
         """
-        for i, feed in enumerate(self.articles):
+        keywords = {}
+        for i, feed in enumerate(self.feeds):
             for j, article in enumerate(feed):
                 for k, word in enumerate(article.keywords):
-                    pass
+                    if word in keywords:
+                        keywords[word]['count'] += 1
+                    else:
+                        keywords[word]['count'] = 1
+                    keywords[word]['positions'].append([i, j, k])
+        return keywords
 
     @staticmethod
     def get_keywords_score(keywords: dict) -> dict:
