bb600e57ef7e90e58d67b431c85d261295e25ce9
m1n0 <mino.lukac@gmail.com>
2017-06-17 20:33:13 +0200

    Trending articles message

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     # Analyze keywords
     analyzer = Analyzer(entries)
     keywords = analyzer.get_keywords_count()
+    pprint(keywords)
 
     # Get data from analyzer.
     data = analyzer.get_morst_relevant_data(keywords)
@@ -30,3 +31,10 @@ if __name__ == '__main__':
 
     slack_message = "What's happening today: {}\n\n{}".format(article_result.orginal.title, article_result.url)
     request('GET', 'https://slack.com/api/chat.postMessage?token=xoxp-87076484640-87088670897-121376898595-f020c7663ad965c5ed1a3722bcec15a8&channel=python-hacks&text={}&pretty=1'.format(slack_message))
+
+    titles = "\n".join([article.orginal.title for article in data['trending_articles']])
+    pprint(titles)
+    slack_message = "Other trending articles: \n\n{}".format(titles)
+    request('GET',
+            'https://slack.com/api/chat.postMessage?token=xoxp-87076484640-87088670897-121376898595-f020c7663ad965c5ed1a3722bcec15a8&channel=python-hacks&text={}&pretty=1'.format(
+                slack_message))
